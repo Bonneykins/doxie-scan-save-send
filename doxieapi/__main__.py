0908dc42bb8fd114314283a1e2bfece5376114c3
Samuel Bonney <bonneysamuel@gmail.com>
2016-08-08 11:30:36 +1200

    Added smtp for file sending

--- doxieapi/__main__.py
+++ doxieapi/__main__.py
@@ -1,16 +1,31 @@
 import os
 
 from .api import DoxieScanner
+from .smtp import send_mail
+
 
 def main():
     """
     Grab all available scan images and save them to the current working directory
     """
-    for doxie in DoxieScanner.discover():
-        print("Discovered {}.".format(doxie))
-        for scan in doxie.download_scans_renamed(os.getcwd()):
+    scanners = DoxieScanner.discover()
+    for scanner in scanners:
+        print("Discovered {}.".format(scanner))
+        for scan in scanner.download_scans_renamed(os.getcwd()):
             print("Saved {}".format(scan))
-
+            send_mail(  send_from='1494hardware@gmail.com',
+                        send_to='sam@1494.co.nz',
+                        subject='Test Subject',
+                        text='Scan from a Wild Doxie',
+                        files=[scan],
+                        server="smtp.gmail.com",
+                        port=587,
+                        username='1494hardware@gmail.com',
+                        password='hardware1',
+                        isTls=True)
+            print("Sent {}".format(scan))
+        scanner.delete_scans([scan['name'] for scan in scanner.scans])
+        print("Deleted scans")
 
 if __name__ == '__main__':
     main()
---
+++ doxieapi/smtp.py
@@ -0,0 +1,28 @@
+import smtplib, os
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
+
+def send_mail( send_from, send_to, subject, text, files=[], server="localhost", port=587, username='', password='', isTls=True):
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime = True)
+    msg['Subject'] = subject
+
+    msg.attach( MIMEText(text) )
+
+    for f in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload( open(f,"rb").read() )
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server, port)
+    if isTls: smtp.starttls()
+    smtp.login(username,password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
